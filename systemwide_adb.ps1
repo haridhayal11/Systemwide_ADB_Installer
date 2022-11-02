4c2545e143ba82f963a4b5e2dad6398923e80096
haridhayal11 <haridhayal@gmail.com>
2022-11-02 18:13:34 +0530

    Fix script on Windows 10 & clear text outputs

--- systemwide_adb.ps1
+++ systemwide_adb.ps1
@@ -2,25 +2,30 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 $host.ui.RawUI.WindowTitle = 'ADB SYSTEM INSTALLER by Haridhayal'
 
 write-host "Downloading ADB"
-$DownloadDestinationADB = "$PSScriptRoot\platform-tools-latest-windows.zip"
-$UnzipDestinationADB = "$PSScriptRoot\platform-tools-latest-windows"
+$DownloadDestinationADB = "$HOME\platform-tools-latest-windows.zip"
+$UnzipDestinationADB = "$HOME\platform-tools-latest-windows"
 $url = "https://dl.google.com/android/repository/platform-tools-latest-windows.zip"
 $start_time = Get-Date
-[string]$sourceDirectory = "$PSScriptRoot\ADB"
+[string]$sourceDirectory = "$HOME\ADB"
      
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $DownloadDestinationADB
 
 Write-Host "Unzipping and Copying"
 Expand-Archive -Path $DownloadDestinationADB -DestinationPath $UnzipDestinationADB 
-[string]$sourceDirectory = "$PSScriptRoot\platform-tools-latest-windows\platform-tools\"
-[string]$destinationDirectory = "$PSScriptRoot\ADB"
+[string]$sourceDirectory = "$HOME\platform-tools-latest-windows\platform-tools\"
+[string]$destinationDirectory = "$HOME\ADB"
 Remove-item $destinationDirectory -Recurse -Force -ErrorAction SilentlyContinue
 Copy-item -Force -Recurse $sourceDirectory -Destination $destinationDirectory
 Write-Host "Cleaning up"
-$RemoveADBFiles = "$PSScriptRoot\platform-tools-latest-windows.zip", "$PSScriptRoot\platform-tools-latest-windows"
+$RemoveADBFiles = "$HOME\platform-tools-latest-windows.zip", "$HOME\platform-tools-latest-windows"
 Remove-Item $RemoveADBFiles -Recurse -Force
 [Environment]::SetEnvironmentVariable("PATH", "$ENV:PATH;$destinationDirectory", "MACHINE")
 
-Write-Host "Completed Installing ADB systemwide"
+Write-Host " "
+Write-Host " "
+Write-Host "Completed Installing ADB systemwide" -ForegroundColor Green
+Write-Host " "
+Write-Host " "
 Write-Host "Launch Windows Terminal/Powershell/Command Prompt to use ADB"
+Write-Host " "
