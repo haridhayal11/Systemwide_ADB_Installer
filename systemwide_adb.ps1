74d166b48374cd0e5a47add1090ffbb0c3797c74
Charlie Quick <cquickreal00@gmail.com>
2023-11-15 15:12:47 -0500

    Update systemwide_adb.ps1
    Moved all variables to the top (after security check) and organized everything into sections with commented headings.

--- systemwide_adb.ps1
+++ systemwide_adb.ps1
@@ -1,26 +1,35 @@
+# --- security check --- #
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 $host.ui.RawUI.WindowTitle = 'ADB SYSTEM INSTALLER by Haridhayal'
 
-write-host "Downloading ADB"
+# --- variables --- #
 $DownloadDestinationADB = "$HOME\platform-tools-latest-windows.zip"
 $UnzipDestinationADB = "$HOME\platform-tools-latest-windows"
 $url = "https://dl.google.com/android/repository/platform-tools-latest-windows.zip"
 [string]$sourceDirectory = "$HOME\ADB"
-     
+[string]$sourceDirectory = "$HOME\platform-tools-latest-windows\platform-tools\"
+[string]$destinationDirectory = "$HOME\ADB"
+$RemoveADBFiles = "$HOME\platform-tools-latest-windows.zip", "$HOME\platform-tools-latest-windows"
+
+# --- download adb --- #
+write-host "Downloading ADB"
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $DownloadDestinationADB
 
+# --- unzip adb download and move it to desired location --- #
 Write-Host "Unzipping and Copying"
 Expand-Archive -Path $DownloadDestinationADB -DestinationPath $UnzipDestinationADB 
-[string]$sourceDirectory = "$HOME\platform-tools-latest-windows\platform-tools\"
-[string]$destinationDirectory = "$HOME\ADB"
 Remove-item $destinationDirectory -Recurse -Force -ErrorAction SilentlyContinue
 Copy-item -Force -Recurse $sourceDirectory -Destination $destinationDirectory
+
+# --- cleanup adb download files --- #
 Write-Host "Cleaning up"
-$RemoveADBFiles = "$HOME\platform-tools-latest-windows.zip", "$HOME\platform-tools-latest-windows"
 Remove-Item $RemoveADBFiles -Recurse -Force
+
+# --- set adb environment variable --- #
 [Environment]::SetEnvironmentVariable("PATH", "$ENV:PATH;$destinationDirectory", "MACHINE")
 
+# --- complete message --- #
 Write-Host " "
 Write-Host " "
 Write-Host "Completed Installing ADB systemwide" -ForegroundColor Green
